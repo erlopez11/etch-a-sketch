0532a82532805a3ab7a40dcd8fe77660ad61d929
Your Nameerlopez11 <lopezelainy@gmail.com>
2023-06-08 10:10:47 -0500

    function for creating grid using rows and columns

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const container = document.querySelector('.grid_container');
 
-function addSquare(numSquares) {
+/*function addSquare(numSquares) {
     for (let i = 0; i < numSquares; i++) {
         const content = document.createElement('div');
         content.classList.add('content');
@@ -8,4 +8,19 @@ function addSquare(numSquares) {
     }
 }
 
-addSquare(256);
+addSquare(256);*/
+
+function createGrid(rows, columns) {
+    for(let i = 0; i < rows; i++) {
+        let row = document.createElement('div');
+        row.classList.add('row');
+        for (let j = 0; j < columns; j++) {
+            let column = document.createElement('div');
+            column.classList.add('column');
+            row.appendChild(column);
+        }
+        container.appendChild(row)
+    }
+}
+
+createGrid(16, 16);
