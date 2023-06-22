dc162ebe23978e59bb938da34b0b2a91b006da06
Your Nameerlopez11 <lopezelainy@gmail.com>
2023-06-22 10:12:11 -0500

    connect value slider to the function that creates the grid: bug-the first square in grid does not change color

--- script.js
+++ script.js
@@ -1,20 +1,20 @@
 const grid_container = document.querySelector('.grid_container');
+const slider = document.querySelector('.slider');
+const output = document.querySelector('.slider_value');
+output.innerHTML = slider.value;
 
-function getGridSize(gridSize) {
-    gridSize = prompt('What size grid would you like?')
 
-    if (gridSize > '64') {
-        prompt("The grid size cannot be greater than 64.");
-    } else {
-        return parseInt(gridSize);
-    }
+//update grid size value
+slider.oninput = function() {
+    return output.innerHTML = this.value;
 }
 
+//Create the Sketch Grid Based on Desired Number
 function createGrid(grid_number) {
     grid_container.style.gridTemplateColumns = `repeat(${grid_number}, 1fr)`;
     grid_container.style.gridTemplateRows =  `repeat(${grid_number}, 1fr)`;
 
-    for (let i = 0; i < grid_number * grid_number; i++) {
+    for (let i = 0; i <= grid_number * grid_number; i++) {
         let square = document.createElement('div');
         square.classList.add('grid_square');
         grid_container.appendChild(square);
@@ -25,27 +25,19 @@ function createGrid(grid_number) {
     }
 }
 
-
+//Square Color Change on MouseOver
 function changeColor(event) {
     this.style.backgroundColor = '#011999';
 }
 
-
-/*var color = [, "#3C9EE7", "#E7993C",
-"#E73C99", "#3CE746", "#E7993C"];
-
-document.querySelector("div").addEventListener(
-"mouseover", function () {
-    document.querySelector(".grid_container div").style.background
-= color[(Math.floor(Math.random(".grid_container div") * color.length))];
-})*/
-
-//createGrid(50);
-
+//Create the Canvas
 function createCanvas() {
-    let gridSize = getGridSize();
-    createGrid(gridSize);
+    let squares = grid_container.querySelectorAll('div');
+    squares.forEach(squares => squares.remove());
+    createGrid(slider.value);
 }
 createCanvas();
+
+slider.addEventListener('mouseup', createCanvas);
 
 
